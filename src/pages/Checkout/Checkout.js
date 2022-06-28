fd03526572f2f77204db2dde8a28a1f8e3adfe75
JefranulIslam <jefranulislam@gmail.com>
2022-06-29 00:09:30 +0600

    Readme .md file edit

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -23,14 +23,7 @@ const Checkout = () => {
                         <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 ">Phone number</label>
                         <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
                     </div>
-                    <div>
-                        <label for="website" class="block mb-2 text-sm font-medium text-gray-900 ">Website URL</label>
-                        <input type="url" id="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="flowbite.com" required />
-                    </div>
-                    <div>
-                        <label for="visitors" class="block mb-2 text-sm font-medium text-gray-900 ">Unique visitors (per month)</label>
-                        <input type="number" id="visitors" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="" required />
-                    </div>
+
                 </div>
                 <div class="mb-6">
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Email address</label>
